97bd92c7adf49f47f8a52513341282167ab30386
ENDERastronaute <95177347+ENDERastronaute@users.noreply.github.com>
2022-12-24 22:28:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const cube = document.querySelector(".cube")
+let isMobile = window.matchMedia("(pointer:coarse)").matches;
 
-window.addEventListener('mousemove', function(evt) {
+if (isMobile) {
+
+    window.addEventListener('pointermove', function(evt) {
+        cube.style.animation = "animate 0s linear infinite"
+        cube.style.transform = "rotateX(" + -evt.clientY +"deg) rotateY(" + evt.clientX +"deg)"
+    })
+
+} else {
+
+    window.addEventListener('mousemove', function(evt) {
         
-    cube.style.animation = "animate 0s linear infinite"
-    cube.style.transform = "rotateX(" + -evt.clientY +"deg) rotateY(" + evt.clientX +"deg)"
-    },
-)
+        cube.style.animation = "animate 0s linear infinite"
+        cube.style.transform = "rotateX(" + -evt.clientY +"deg) rotateY(" + evt.clientX +"deg)"
+        },
+    )
+    
+    window.addEventListener('mouseout', function() {
+        cube.style.animation = "animate 4s linear infinite"
+    })
+}
 
-window.addEventListener('mouseout', function() {
-    cube.style.animation = "animate 4s linear infinite"
-})
+
